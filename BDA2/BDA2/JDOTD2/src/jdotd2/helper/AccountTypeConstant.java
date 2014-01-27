b779256b66d8123a87187d5a185647aef7d01d3f
CESCHEL Marvin <Marvin.Ceschel@polytech.unice.fr>
2014-01-27 16:49:55 +0100

    Revert "upload project"
    This reverts commit c48bf14d617c5246fab7fd8cf7b5bd5d9b15ec31.

--- BDA2/BDA2/JDOTD2/src/jdotd2/helper/AccountTypeConstant.java
+++
@@ -1,8 +0,0 @@
-package jdotd2.helper;
-
-public class AccountTypeConstant {
-	public static String STANDARD_PARTICULAR="STANDARD_PARTICULAR";
-	public static String PRIVILEGE="PRIVILEGE";
-	public static String STANDARD_ENTERPRISE="STANDARD_ENTERPRISE";
-	public static String REWARDING="REWARDING";
-}
